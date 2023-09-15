a2b2a9f085a764b8c44fe27a19dedb862fb99124
Boyeon-Shin <20221023@o365.hanbat.ac.kr>
2023-09-15 21:56:12 +0900

    sss

--- chap01/src/chap01/Hello.java
+++ chap01/src/chap01/Hello.java
@@ -3,7 +3,7 @@ package chap01;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, wekcome to the java world");
+		System.out.println("Hello, wekcome to the java world haha ");
 
 	}
 
